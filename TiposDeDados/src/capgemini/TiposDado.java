a19d81d36eefa51d5597865d018b57ea2ee13f5b
teste aula kotlin <victorhugo.soares20@gmail.com>
2025-03-27 16:16:30 -0300

    Primeiro commit

---
+++ TiposDeDados/src/capgemini/TiposDado.java
@@ -0,0 +1,28 @@
+package capgemini;
+
+public class TiposDado {
+
+	public static void main(String[] args) {
+		// demonstração de alguns tipos de dados
+		int numero;
+		float decimalCurto;
+		double decimalLongo;
+		char caracter;
+		String cadeiaCaracteres;
+		
+		numero = 10;
+		decimalCurto = 10.5f;
+		decimalLongo = 20.5f;
+		caracter = 'A';
+		cadeiaCaracteres = "Curso Java Fundamentos CapGemini";
+		
+		
+		System.out.println(numero);
+		System.out.println(decimalCurto);
+		System.out.println(decimalLongo);
+		System.out.println(caracter);
+		System.out.println(cadeiaCaracteres);
+		
+	}
+
+}
