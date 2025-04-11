767f1b52b7e3b3bb4aa52bffc97751bd5f999d5d
Victor Hugo <victorhugo.soares19@gmail.com>
2025-04-11 15:15:46 -0300

    [Tipo: Feature/Fix/Refactor] Meu Programa Capgemini
     Descrição detalhada (opcional): Conteudos das Aula de Java - Meu
    Primeiro Programa
    
    - O que foi alterado? Nada
    
    - Por que foi alterado?
    
    Issue relacionada (ex: #123)

---
+++ MeuProgramaCapgemini/src/capgemini/ClassePrincipal.java
@@ -0,0 +1,12 @@
+package capgemini;
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		final float numero = 855.6f;
+		System.out.println("Valor de numero é : " + numero);
+		
+		
+	}
+
+}
---
+++ MeuProgramaCapgemini/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MeuProgramaCapgemini {
+}
