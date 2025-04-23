3160b56f82b094fa8712471494e879bcf5845746
Victor Hugo <victorhugo.soares19@gmail.com>
2025-04-23 09:55:55 -0300

    [Tipo: Feature/Fix/Refactor] Literais (máx. 50 caracteres)
     Descrição detalhada (opcional): Conteudo do curso da Capgemini START
    - O que foi alterado? Projeto Completo
     - Por que foi alterado?

---
+++ Literais/src/capgemini/Literal.java
@@ -0,0 +1,23 @@
+package capgemini;
+
+public class Literal {
+
+	public static void main(String[] args) {
+		int numero;
+		String cadeiaCaracteres;
+		char caracter;
+		boolean certoOuErrado;
+		
+		numero = 10;
+		cadeiaCaracteres = "Curso Java Fundamentos CapGemini";
+		caracter = 'S';
+		certoOuErrado = true;
+		
+		System.out.println("O valor de numero é: " + numero);
+		System.out.println("O valor de cadeia de caracteres é: " + cadeiaCaracteres);
+		System.out.println("O valor de caracter é: " + caracter);
+		System.out.println("O valor de certo ou errado é: " + certoOuErrado);
+		
+	}
+
+}
